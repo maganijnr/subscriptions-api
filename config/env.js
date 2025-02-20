f9eb00deea5212699992e2d6e4d5097ca23a4f5f
Joshua Magani <maganijoshua25@gmail.com>
2025-02-20 12:08:59 +0100

    env fix

--- config/env.js
+++ config/env.js
@@ -1,7 +1,7 @@
 import { config } from "dotenv";
 
 config({
-	path: `.env.${process.env.NODE_ENV || "development"}.local`,
+	path: `.env.${process.env.NODE_ENV || "dev"}.local`,
 });
 
 export const { PORT, DB_URL, NODE_ENV, JWT_SECRET, JWT_EXPIRES_IN } =
